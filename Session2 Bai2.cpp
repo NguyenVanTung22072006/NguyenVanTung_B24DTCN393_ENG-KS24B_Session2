2b74b85c5d77187aa6f0ba579b71a8c1fee7cda3
NguyenVanTung22072006 <nvtung2207@gmail.com>
2024-11-20 22:40:19 +0700

    Session2 Bai2

---
+++ Session2 Bai2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Khai báo và kh?i t?o hai bi?n s? nguyên
+    int num1 = 15; // S? nguyên th? nh?t
+    int num2 = 20; // S? nguyên th? hai
+
+    // Khai báo và tính t?ng c?a hai s?
+    int sum = num1 + num2; // Tính t?ng và luu vào bi?n 'sum'
+
+    // In k?t qu? ra màn hình
+    printf("S? th? nh?t: %d\n", num1);
+    printf("S? th? hai: %d\n", num2);
+    printf("T?ng c?a hai s? là: %d\n", sum);
+
+    return 0;
+}
